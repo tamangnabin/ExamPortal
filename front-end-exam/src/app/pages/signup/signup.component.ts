839a52fa8fd3140ec530fe2e31be4f71d2597f4a
Mohan KC <mohan.kc9841@gmail.com>
2021-09-20 10:58:59 -0500

    added angular

---
+++ front-end-exam/src/app/pages/signup/signup.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+
+  public user={
+    username:'',
+    password:'',
+    firstName:'',
+    lastName:'',
+    email:'',
+    phone:''
+  };
+
+  ngOnInit(): void {}
+
+  formSubmit(){
+    alert('submit');
+  }
+
+}
