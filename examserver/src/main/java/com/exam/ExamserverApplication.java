865ce4075ad5f61f39bbf7242e1fd15be67fd3a1
Kcmohan1997 <54957060+Kcmohan1997@users.noreply.github.com>
2021-09-20 11:22:29 -0500

    Update ExamserverApplication.java

--- examserver/src/main/java/com/exam/ExamserverApplication.java
+++ examserver/src/main/java/com/exam/ExamserverApplication.java
@@ -23,32 +23,4 @@ public class ExamserverApplication {
 		SpringApplication.run(ExamserverApplication.class, args);
 	}
 
-//	@Override
-//	public void run(String... args) throws Exception {
-//
-//		System.out.println("Adding user...");
-//
-//		User user = new User();
-//		user.setUsername("kamal123");
-//		user.setPassword("Nepal123");
-//		user.setFirstName("Kamal");
-//		user.setLastName("Kunwar");
-//		user.setEmail("kamal.kunwar0@gmail.com");
-//		user.setProfile("default.png");
-//
-//		Role role1 = new Role();
-//		role1.setRoleId(44L);
-//		role1.setRoleName("ADMIN");
-//
-//		Set<UserRole> userRoleSet = new HashSet<UserRole>();
-//		UserRole userRole = new UserRole();
-//		userRole.setRole(role1);
-//		userRole.setUser(user);
-//
-//		userRoleSet.add(userRole);
-//
-//		User user1 = this.userService.createUser(user, userRoleSet);
-//		System.out.println(user1.getUsername());
-//	}
-
 }
