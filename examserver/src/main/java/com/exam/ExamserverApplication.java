64b72988e00491b8289d611d322dba36c2880f8f
nabin <nabinblon4@gmail.com>
2022-11-05 23:22:31 -0400

    This a commit 1

--- examserver/src/main/java/com/exam/ExamserverApplication.java
+++ examserver/src/main/java/com/exam/ExamserverApplication.java
@@ -20,7 +20,6 @@ public class ExamserverApplication {
 	private UserService userService;
 
 	public static void main(String[] args){
-		System.out.println("This is a Test");
 		SpringApplication.run(ExamserverApplication.class, args);
 	}
 
